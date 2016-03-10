437fa121f06c52972f86d82563bb589decf7b502
joshuafbadams <josh.f.b.adams@gmail.com>
2016-03-10 03:45:19 +0000

    data sync

--- public/src/js/controllers/mainCtrl.js
+++ public/src/js/controllers/mainCtrl.js
@@ -2,4 +2,12 @@
       .controller('mainController', function($scope) {
 
 	//Main script variables
+	$scope.background= [
+		{image: 'dist/img/buzz_background.png', description: 'main background'},
+		{image: 'dist/img/buzz_logo/img_full.png', description: 'main logo'},
+		{image: 'dist/img/buzz_logo/img_full_down.png', description: 'main down full'},
+		{image: 'dist/img/buzz_logo/letters_only.png', description: 'letters'},
+		{image: 'dist/img/buzz_logo/main.png', description: 'main bee only'},
+		{image: 'dist/img/buzz_logo/spray_paint.png', description: 'bee spray paint'}
+	];
 });
