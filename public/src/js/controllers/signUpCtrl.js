436854a33599472b14ce58dd3c2fff3147690454
joshuafbadams <josh.f.b.adams@gmail.com>
2016-03-16 14:22:21 +0000

    git sync

--- public/src/js/controllers/signUpCtrl.js
+++ public/src/js/controllers/signUpCtrl.js
@@ -4,9 +4,16 @@ angular.module('signUpCtrl', [])
 
     //second page angular variables go here
 	$scope.master = {};
-      	$scope.update = function(user) {
-        	$scope.master = angular.copy(user);
+      	$scope.update = function(signUp) {
+       	   if(err)	
+	     return console.log(err);
       };
-
+        $http.post("/signUp").success(function(data) {
+        // success
+        console.log('Successful post');
+        }).error(function(error) {
+        // error
+        console.log(JSON.stringify("Failed to get t&c: " + error));
+        });
     }]);
     
