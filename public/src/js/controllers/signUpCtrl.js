138de17efaadf6079764f6d05731c6cc0b41d828
joshuafbadams <josh.f.b.adams@gmail.com>
2016-03-16 16:51:48 +0000

    angular changes

--- public/src/js/controllers/signUpCtrl.js
+++ public/src/js/controllers/signUpCtrl.js
@@ -3,17 +3,6 @@ angular.module('signUpCtrl', [])
 	.controller('signUpController', ['$scope', function($scope) {
 
     //second page angular variables go here
-	$scope.master = {};
-      	$scope.update = function(signUp) {
-       	   if(err)	
-	     return console.log(err);
-      };
-        $http.post("/signUp").success(function(data) {
-        // success
-        console.log('Successful post');
-        }).error(function(error) {
-        // error
-        console.log(JSON.stringify("Failed to get t&c: " + error));
-        });
+	
     }]);
     
